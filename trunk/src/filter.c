8277b79829d2d8b3a83fe8f4149b898c21c3ce46
jon.turney@dronecode.org.uk <jon.turney@dronecode.org.uk@dccc4ef9-3752-0410-9079-47cdceed9760>
2010-03-02 15:42:24 +0000

    Always apply printable character filter to track information
    (issue 198)

--- trunk/src/filter.c
+++ trunk/src/filter.c
@@ -5,10 +5,10 @@
 #include "utils.h"
 #include "musictracker.h"
 
-/* Filters out all black-listed words, as well as non-printable characters
+/* Filters out all black-listed words
  */
 void
-filter(char *str)
+filter_profanity(char *str)
 {
         gboolean changed = FALSE;
 
@@ -17,7 +17,6 @@ filter(char *str)
 	char **list = g_strsplit(purple_prefs_get_string(PREF_FILTER),
 			",", 0);
 
-
 	char **p;
 	for (p = list; *p != 0; ++p) {
                 int len = strlen(*p);
@@ -25,7 +24,7 @@ filter(char *str)
                   continue;
 
                 // construct a regex pattern which matches the distinct word
-                // i.e. use word boundaries to avoid matchin as a substring...
+                // i.e. use word boundaries to avoid matching as a substring...
                 char pattern[len+10];
                 sprintf(pattern, "\\b(%s)\\b", *p);
                 pcre *re = regex(pattern, PCRE_CASELESS | PCRE_UTF8);
@@ -38,30 +37,47 @@ filter(char *str)
                     changed = TRUE;
                   }
 
-                pcre_free(re);                
+                pcre_free(re);
 	}
 	g_strfreev(list);
-
-	// Remove unprintable chars
-        char *u = str;
-        while (*u != 0)
-          {
-            gunichar c = g_utf8_get_char(u);
-            gchar *next = g_utf8_next_char(u);
-            if (!g_unichar_isprint(c))
-              {
-                for (; u < next; u++)  
-                  *u = mask;
-                changed = TRUE;
-              }
-            u = next;
-          }
 
         // debug report that filtering happened
         if (changed)
           {
-              trace("filtered to: %s", str);
+              trace("profanity filtered to: %s", str);
           }
+}
+
+/*
+   Filters out non-printable characters
+ */
+void
+filter_printable(char *str)
+{
+  gboolean changed = FALSE;
+
+  char mask = *purple_prefs_get_string(PREF_MASK);
+
+  // Remove unprintable chars
+  char *u = str;
+  while (*u != 0)
+    {
+      gunichar c = g_utf8_get_char(u);
+      gchar *next = g_utf8_next_char(u);
+      if (!g_unichar_isprint(c))
+        {
+          for (; u < next; u++)
+            *u = mask;
+          changed = TRUE;
+        }
+      u = next;
+    }
+
+  // debug report that filtering happened
+  if (changed)
+    {
+      trace("printable filtered to: %s", str);
+    }
 }
 
 const char*
